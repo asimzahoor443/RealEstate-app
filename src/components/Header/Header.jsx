7633693847abe13d8d5b6f165f9d477b9efbaa34
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2024-11-19 12:21:34 +0500

    initial updates

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const Header = () => {
+  return (
+    <Section className="h-wrapper">
+      <div className="h-conatiner">
+        <img src="" alt="" />
+      </div>
+    </Section>
+  );
+};
+
+export default Header;
